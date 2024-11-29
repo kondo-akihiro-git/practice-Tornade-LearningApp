5799a4c35a04d58e02feb8a5a5fe7ca2baa22826
akihiro-kondo <2107sum@gmail.com>
2024-11-30 03:16:58 +0900

    Update: ディレクトリ構成変更

--- app.py
+++ app.py
@@ -1,40 +1,27 @@
 import tornado.ioloop
 import tornado.web
-import os
 
-# ハンドラークラスを作成
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render("index.html", message="Welcome to the Learning Site!")
+        self.write("Hello, Tornado!")
 
-class LessonHandler(tornado.web.RequestHandler):
+class ApiHandler(tornado.web.RequestHandler):
+    def prepare(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+        self.set_header("Access-Control-Allow-Headers", "Content-Type")
+    
     def get(self):
-        self.render("lesson.html", lesson_title="Lesson 1: Introduction to Python")
+        self.write({"message": "Hello from Tornado!"})
 
-class AboutHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.render("about.html", message="This is the About Page")
-
-class ContactHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.render("contact.html", message="Contact Us Here")
-
-
-# Tornadoの設定
 def make_app():
-    return tornado.web.Application(
-        [
-            (r"/", MainHandler),
-            (r"/lesson", LessonHandler),
-            (r"/about", AboutHandler),
-            (r"/contact", ContactHandler),
-        ],
-        template_path=os.path.join(os.path.dirname(__file__), "templates"),
-        static_path=os.path.join(os.path.dirname(__file__), "static"),
-    )
-
+    return tornado.web.Application([
+        (r"/", MainHandler),
+        (r"/api/data", ApiHandler),
+    ])
 
 if __name__ == "__main__":
     app = make_app()
-    app.listen(8888)  # ポート8888でアプリケーションを実行
-    tornado.ioloop.IOLoop.current().start()  # Tornadoの非同期ループを開始
+    app.listen(8888)
+    print("Tornado is running at http://localhost:8888")
+    tornado.ioloop.IOLoop.current().start()
